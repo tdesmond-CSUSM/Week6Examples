9c47b84213361a28dafeeac42f745a9f119d9210
Thomas Desmond <thomasdes533@gmail.com>
2017-10-04 14:38:08 -0700

    Added a pin to the map.

--- Week6Examples/MapSamplePage.xaml.cs
+++ Week6Examples/MapSamplePage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Week6Examples
 {
@@ -10,6 +11,25 @@ namespace Week6Examples
         public MapSamplePage()
         {
             InitializeComponent();
+            //MySampleMap.MoveToRegion(new MapSpan(new Position(33.1307785, -117.1601826), 33.1307785, -117.1601826 ));
+            var initialMapLocation = MapSpan.FromCenterAndRadius(new Position(33.1307785, -117.1601826), Distance.FromMiles(1));
+            MySampleMap.MoveToRegion(initialMapLocation);
+
+
+			PlaceAMarker();
+        }
+
+        private void PlaceAMarker()
+        {
+            var position = new Position(33.1307785, -117.1601826); // Latitude, Longitude
+			var pin = new Pin
+			{
+				Type = PinType.Place,
+				Position = position,
+				Label = "custom pin",
+				Address = "custom detail info"
+			};
+            MySampleMap.Pins.Add(pin);
         }
     }
 }
