0c4f7eab7fec277db945e1aa76cff35649cf0ff1
Thomas Desmond <thomasdes533@gmail.com>
2017-10-03 19:48:04 -0700

    Initial Commit. Added first basic map iOS only.

---
+++ Week6Examples/MapSamplePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Week6Examples
+{
+    public partial class MapSamplePage : ContentPage
+    {
+        public MapSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Week6Examples/Week6ExamplesPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Week6Examples
+{
+    public partial class Week6ExamplesPage : ContentPage
+    {
+        public Week6ExamplesPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_NavigateToMapSample(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MapSamplePage());
+        }
+    }
+}
